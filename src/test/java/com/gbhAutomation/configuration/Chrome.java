2c375c134c1bf8ed152d5c993fb1ca2c249efce2
R Mancebo <robertomancebo@outlook.com>
2019-09-18 20:12:08 -0400

    Cleaning code and new ChromDriver added in order to test the reason why is opening two Browsers, issue persist by the ChromeDriver + Chrome version

--- src/test/java/com/gbhAutomation/configuration/Chrome.java
+++ src/test/java/com/gbhAutomation/configuration/Chrome.java
@@ -4,11 +4,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Chrome {
-	
-	public static void startChrome(String url) throws InterruptedException {
-		//this driver method opens a Chrome WebBrowser with the url specified by parameter (Version 76 due the chrome driver version), also, the driver used is for Mac
-		
-		System.setProperty("webdriver.chrome.driver","./src/test/java/com/practica/util/chromedriver");
+
+	//this driver method opens a Chrome WebBrowser with the url specified by parameter (Version 76 due the chrome driver version), also, the driver used is for Mac
+	//NOTE: For some reason the version of Chrome + the version of ChromeDriver that i used is malfunctioning and is opening 2 browsers
+	public void startChrome(String url) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver","./src/test/java/com/gbhAutomation/util/chromedriver");
 		WebDriver driver = new ChromeDriver();
 		driver.get(url);
 		driver.manage().window().maximize();
--- src/test/java/com/gbhAutomation/tests/FireBaseApp.java
+++ src/test/java/com/gbhAutomation/tests/FireBaseApp.java
@@ -1,36 +1,24 @@
 package com.gbhAutomation.tests;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
 
 import com.gbhAutomation.configuration.Chrome;
 
 public class FireBaseApp {
 	
-	WebDriver driver;
 	//I like to use JUnit because it keep it simple, i know that cucumber is easier to read but in my opinion with JUnit its easier to code, just the Pagefactory and the test
 	//Also, with JUnit we can do stuff like specify the method that would be executed at the beginning of every test, we can specify the order by number, and much more
 	
+	//this class opens calls the configuration class (Chrome class that i create on java/com/gbhAutomation/configuration folder)
 	@Before
 	public void startChrome() throws InterruptedException {
-		//this class opens calls the configuration class (Chrome class that i create on java/com/practica/configuration folder)
+		Chrome chrome = new Chrome();
 		String url = "https://gbhqatest.firebaseapp.com/";
-		Chrome.startChrome(url);
-		
+		chrome.startChrome(url);
 	}
 	
+	//automation should be implemented here, on the "@Test" class (can be more than one)
 	@Test
 	public void testExample() {
-		//automation should be implemented here, on the "@Test" class (can be more than one)
 	}
-	
-	@After
-	public void closeBrowser() {
-		//this method will always be implemented after the test
-		driver.close();
-		driver.quit();
-		
-	}
-
 }
