8ada1a5e5dbbe0e24f6b02c4cdeed7de54052fc3
R Mancebo <robertomancebo@outlook.com>
2020-05-03 15:26:23 -0400

    Commit 3- may 2020

--- src/test/java/com/gbhAutomation/tests/FireBaseApp.java
+++ src/test/java/com/gbhAutomation/tests/FireBaseApp.java
@@ -1,14 +1,18 @@
 package com.gbhAutomation.tests;
 import java.awt.AWTException;
+import java.net.MalformedURLException;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
 
-import com.gbhAutomation.Pages.LoginPage;
-import com.gbhAutomation.Pages.NotePage;
-import com.gbhAutomation.configuration.Chrome;
+//import com.gbhAutomation.Pages.LoginPage;
+//import com.gbhAutomation.Pages.NotePage;
+//import com.gbhAutomation.configuration.Chrome;
+//import com.gbhAutomation.configuration.android;
+import com.gbhAutomation.configuration.Ios;
+
 
 public class FireBaseApp {
 	
@@ -17,25 +21,32 @@ public class FireBaseApp {
 	
 	//this class opens calls the configuration class (Chrome class that i create on java/com/gbhAutomation/configuration folder)
 	
-	private LoginPage login;
-	private NotePage note;
+//	private LoginPage login;
+//	private NotePage note;
 
 //	this method runs before every test is going to run
 	@Before
-	public void startChrome() throws InterruptedException {
-		Chrome chrome = new Chrome();
-		chrome.chromePath();
+	public void startChrome() throws InterruptedException, MalformedURLException {
+//		Chrome chrome = new Chrome();
+//		chrome.chromePath();
+//		android an = new android();
+//		an.properties();
+		Ios ios = new Ios();
+		ios.configuration();
 	}
 	
 	//this is a "New note test" that creates a new note every time it runs
 //	Please add your email and password to login into firebaseapp
 	@Test
 	public void newNoteTest() throws InterruptedException, AWTException {
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://gbhqatest.firebaseapp.com/");
-		login = new LoginPage(driver);
-		login.loginToFireBase(" "," ");
-		note = new NotePage(driver);
-		note.addNote("Title here","This is a note");
+//		WebDriver driver = new ChromeDriver();
+//		driver.get("https://gbhqatest.firebaseapp.com/");
+//		login = new LoginPage(driver);
+//		login.loginToFireBase(" "," ");
+//		note = new NotePage(driver);
+//		note.addNote("Title here","This is a note");
+	}
+	public void android() {
+		
 	}
 }
